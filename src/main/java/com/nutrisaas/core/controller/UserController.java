f558db060439ba4c51d5a1a6dcb23c7114dea132
juanpablomh-dev <juanpablo.mh5@gmail.com>
2026-02-23 10:45:02 +0100

    add swagger documentation in user endpoint

--- src/main/java/com/nutrisaas/core/controller/UserController.java
+++ src/main/java/com/nutrisaas/core/controller/UserController.java
@@ -1,9 +1,16 @@
 package com.nutrisaas.core.controller;
 
+import com.nutrisaas.core.dto.ApiError;
 import com.nutrisaas.core.dto.RegisterRequest;
 import com.nutrisaas.core.dto.UserResponse;
 import com.nutrisaas.core.model.User;
 import com.nutrisaas.core.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,6 +26,35 @@ public class UserController {
         this.userService = userService;
     }
 
+    @Operation(
+            summary = "User register",
+            description = "Register a new user."
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Register successful",
+                    content = @Content(schema = @Schema(implementation = UserResponse.class))),
+            @ApiResponse(
+                    responseCode = "409",
+                    description = "Email Already Exists",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class),
+                            examples = @ExampleObject(
+                                    name = "EmailAlreadyExists",
+                                    value = """
+                                            {
+                                              "timestamp": "2026-02-21T18:25:43.511Z",
+                                              "status": 409,
+                                              "error": "Conflict",
+                                              "message": "The email is already registered: user@example.com"
+                                            }
+                                            """
+                            )
+                    )
+            )
+    })
     @PostMapping("/register")
     public ResponseEntity<UserResponse> register(@RequestBody RegisterRequest request) {
         User newUser = userService.register(request);
--- src/main/java/com/nutrisaas/core/dto/UserResponse.java
+++ src/main/java/com/nutrisaas/core/dto/UserResponse.java
@@ -1,10 +1,14 @@
 package com.nutrisaas.core.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
 public class UserResponse {
+    @Schema(description = "User email address", example = "user@example.com")
     private String email;
+    @Schema(description = "User fullName ", example = "Juan Pablo Muñoz")
     private String fullName;
+    @Schema(description = "Status (enabled yes or not)", example = "true")
     private Boolean enabled;
 }
